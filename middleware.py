b03fb7c524c4a4d5d6cb100b60cba1def6a20074
bryan <bryan@31f25ff3-6213-0410-84f9-a6f065a847f4>
2008-11-05 10:13:57 +0000

    factorlib: Minor changes for Django 1.0 compatibility.
    git-svn-id: svn+ssh://s1.fullfactor.com/var/local/svn/fullfactor/factorlib@1008 31f25ff3-6213-0410-84f9-a6f065a847f4

--- middleware.py
+++ middleware.py
@@ -9,7 +9,7 @@ class PageFallbackMiddleware(object):
         if response.status_code != 404:
             return response # No need to check for a page for non-404 responses.
         try:
-            return page(request, request.path)
+            return page(request, request.path_info)
         # Return the original response if any errors happened. Because this
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
