bdb966c76b14efee0934fe0a6bd56f6508a69818
Bryan Chow <bryan@fullfactor.com>
2011-09-23 18:05:23 -0700

    Fix whitespace in model field declarations

--- pages/models.py
+++ pages/models.py
@@ -20,36 +20,33 @@ SITEMAP_CHANGEFREQ_CHOICES = [
 
 class Page(models.Model):
 
-    url                = models.CharField('URL', max_length=128, help_text='Should have leading and trailing slashes. ')
-    title              = models.CharField(max_length=128)
-    body               = models.TextField(max_length=65536, null=True, blank=True)
-    body_markup        = models.CharField(choices=MARKUP_CHOICES, null=True, default=DEFAULT_MARKUP, max_length=8)
-    site               = models.ForeignKey(Site, default=1)
+    url = models.CharField("URL", max_length=128, help_text="Should have leading and trailing slashes.")
+    title = models.CharField(max_length=128)
+    body = models.TextField(max_length=65536, null=True, blank=True)
+    body_markup = models.CharField(choices=MARKUP_CHOICES, null=True, default=DEFAULT_MARKUP, max_length=8)
+    site = models.ForeignKey(Site, default=1)
 
-    page_title         = models.CharField(max_length=512, null=True, blank=True)
-    meta_description   = models.CharField(max_length=512, null=True, blank=True)
-    meta_keywords      = models.CharField(max_length=512, null=True, blank=True)
+    page_title = models.CharField(max_length=512, null=True, blank=True)
+    meta_description = models.CharField(max_length=512, null=True, blank=True)
+    meta_keywords = models.CharField(max_length=512, null=True, blank=True)
     sitemap_changefreq = models.CharField(null=True, blank=True, max_length=50, choices=SITEMAP_CHANGEFREQ_CHOICES)
-    sitemap_priority   = models.DecimalField(null=True, blank=True, max_digits=2, decimal_places=1, help_text='Enter a value from 0.0 to 1.0. ')
+    sitemap_priority = models.DecimalField(null=True, blank=True, max_digits=2, decimal_places=1, help_text="Enter a value from 0.0 to 1.0.")
 
-    template_name      = models.CharField(max_length=128, null=True, blank=True, help_text='If left blank, \'pages/default.html\' will be used. ')
-    visible            = models.BooleanField(default=True)
-    notes              = models.TextField(max_length=16384, null=True, blank=True, help_text='Private notes. ')
-    created            = models.DateTimeField(default=datetime.now())
-    modified           = models.DateTimeField(blank=True)
+    template_name = models.CharField(max_length=128, null=True, blank=True, help_text="If left blank, 'pages/default.html' will be used.")
+    visible = models.BooleanField(default=True)
+    notes = models.TextField(max_length=16384, null=True, blank=True, help_text="Private notes.")
+    created = models.DateTimeField(default=datetime.now())
+    modified = models.DateTimeField(blank=True)
 
-    objects            = models.Manager()
-    on_site            = CurrentSiteManager('site')
+    objects = models.Manager()
+    on_site = CurrentSiteManager('site')
 
     class Meta:
         unique_together = (('url', 'site'),)
-        ordering = ['url',]
+        ordering = ('url',)
 
-    def __repr__(self):
+    def __unicode__(self):
         return self.url
-
-    def __str__(self):
-        return str(self.url)
 
     def get_absolute_url(self):
         return self.url
@@ -64,26 +61,23 @@ class Page(models.Model):
 
 class Redirect(models.Model):
 
-    old_url            = models.CharField('Old URL', max_length=128, unique=True, help_text='Should have a leading slash. ')
-    new_url            = models.CharField('New URL', max_length=128, help_text='Should have leading and trailing slashes. ')
-    site               = models.ForeignKey(Site, default=1)
+    old_url = models.CharField("Old URL", max_length=128, unique=True, help_text="Should have a leading slash.")
+    new_url = models.CharField("New URL", max_length=128, help_text="Should have leading and trailing slashes.")
+    site = models.ForeignKey(Site, default=1)
 
-    notes              = models.TextField(max_length=16384, null=True, blank=True, help_text='Private notes. ')
-    created            = models.DateTimeField(default=datetime.now())
-    modified           = models.DateTimeField(blank=True)
+    notes = models.TextField(max_length=16384, null=True, blank=True, help_text="Private notes.")
+    created = models.DateTimeField(default=datetime.now())
+    modified = models.DateTimeField(blank=True)
 
-    objects            = models.Manager()
-    on_site            = CurrentSiteManager('site')
+    objects = models.Manager()
+    on_site = CurrentSiteManager('site')
 
     class Meta:
         unique_together = (('old_url', 'site'),)
-        ordering = ['old_url',]
+        ordering = ('old_url',)
 
-    def __repr__(self):
+    def __unicode__(self):
         return self.old_url
-
-    def __str__(self):
-        return str(self.old_url)
 
     def get_absolute_url(self):
         return self.new_url
